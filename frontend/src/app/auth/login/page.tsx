3f51694ed005215a0a88a8e597939a199bc1eda4
OrlandoDev17 <orlandodev1707@gmail.com>
2025-10-05 15:16:29 -0400

    🔑 Mejorar páginas de login y registro con nuevas funcionalidades

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useLogin } from "@/hooks/useLogin";
+import { motion, AnimatePresence } from "motion/react";
 
 import { LOGIN_FORM } from "@/lib/constants";
 import { EyeIcon, EyeOffIcon } from "@/ui/Icons";
@@ -29,67 +30,79 @@ export default function LoginPage() {
     login(credentials);
   };
 
+  const variants = {
+    initial: { opacity: 0, y: 40 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -40 },
+  };
+
   return (
     <main className="flex items-center justify-center h-screen">
-      <article
-        className="flex flex-col gap-4 py-10 px-8 border border-gray-500 bg-secondary rounded-lg 
+      <AnimatePresence>
+        <motion.article
+          initial={variants.initial}
+          animate={variants.animate}
+          exit={variants.exit}
+          transition={{ duration: 0.5 }}
+          className="flex flex-col gap-4 py-10 px-8 border border-gray-500 bg-secondary rounded-lg 
       max-w-md w-full"
-      >
-        <div className="flex flex-col items-center gap-4">
-          <h2 className="text-3xl text-blue-500 font-bold">RideNow</h2>
-          <h3 className="text-2xl font-semibold">Iniciar Sesión</h3>
-          <h5 className="text-sm text-gray-500">
-            Ingresa a tu cuenta de RideNow
-          </h5>
-        </div>
-        <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
-          {LOGIN_FORM.map(
-            ({ id, label, type, placeholder, required, name }) => (
-              <label className="flex flex-col gap-2 relative" key={id}>
-                <span className="font-medium">{label}</span>
-                <input
-                  className="px-4 py-2 bg-neutral-800 border border-gray-500 rounded-lg focus:outline-none focus:border-blue-500 transition"
-                  type={isVisible ? "text" : type}
-                  placeholder={
-                    isVisible && type === "password"
-                      ? "Contraseña"
-                      : placeholder
-                  }
-                  required={required}
-                  name={name}
-                />
-                {type === "password" && (
-                  <button
-                    type="button"
-                    onClick={handleTogglePassword}
-                    className="absolute right-2 bottom-2"
-                  >
-                    {isVisible ? <EyeIcon /> : <EyeOffIcon />}
-                  </button>
-                )}
-              </label>
-            )
-          )}
-          <button className="flex items-center justify-center gap-2 px-4 py-2 bg-blue-500 text-white rounded-lg mt-2 hover:bg-blue-600 transition cursor-pointer">
-            Iniciar Sesión
-          </button>
-        </form>
-        <span className="flex items-center justify-center gap-2 text-gray-400 text-sm">
-          ¿No tienes cuenta?
-          <a
-            href="/auth/register"
-            className=" text-blue-400 font-semibold hover:underline transition"
-          >
-            Regístrate Aquí
-          </a>
-        </span>
-        <a
-          className="text-center text-gray-400 text-sm hover:text-white transition"
-          href="/"
         >
-          Volver al Inicio
-        </a>
-      </article>
+          <div className="flex flex-col items-center gap-4">
+            <h2 className="text-3xl text-blue-500 font-bold">RideNow</h2>
+            <h3 className="text-2xl font-semibold">Iniciar Sesión</h3>
+            <h5 className="text-sm text-gray-500">
+              Ingresa a tu cuenta de RideNow
+            </h5>
+          </div>
+          <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
+            {LOGIN_FORM.map(
+              ({ id, label, type, placeholder, required, name }) => (
+                <label className="flex flex-col gap-2 relative" key={id}>
+                  <span className="font-medium">{label}</span>
+                  <input
+                    className="px-4 py-2 bg-neutral-800 border border-gray-500 rounded-lg focus:outline-none focus:border-blue-500 transition"
+                    type={isVisible ? "text" : type}
+                    placeholder={
+                      isVisible && type === "password"
+                        ? "Contraseña"
+                        : placeholder
+                    }
+                    required={required}
+                    name={name}
+                  />
+                  {type === "password" && (
+                    <button
+                      type="button"
+                      onClick={handleTogglePassword}
+                      className="absolute right-2 bottom-2"
+                    >
+                      {isVisible ? <EyeIcon /> : <EyeOffIcon />}
+                    </button>
+                  )}
+                </label>
+              )
+            )}
+            <button className="flex items-center justify-center gap-2 px-4 py-2 bg-blue-500 text-white rounded-lg mt-2 hover:bg-blue-600 transition cursor-pointer">
+              Iniciar Sesión
+            </button>
+          </form>
+          <span className="flex items-center justify-center gap-2 text-gray-400 text-sm">
+            ¿No tienes cuenta?
+            <a
+              href="/auth/register"
+              className=" text-blue-400 font-semibold hover:underline transition"
+            >
+              Regístrate Aquí
+            </a>
+          </span>
+          <a
+            className="text-center text-gray-400 text-sm hover:text-white transition"
+            href="/"
+          >
+            Volver al Inicio
+          </a>
+        </motion.article>
+      </AnimatePresence>
     </main>
   );
 }
--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,3 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import { RegisterForm } from "@/ui/auth/RegisterForm";
+import { AnimatePresence, motion } from "motion/react";
+import {
+  useRegisterContext,
+  RegisterProvider,
+} from "@/context/RegisterContext";
+
+function RegisterPageContent() {
+  const [step, setStep] = useState(1);
+  const { role, setRole } = useRegisterContext();
+
+  const variants = {
+    initial: { opacity: 0, y: 40 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -40 },
+  };
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <AnimatePresence>
+        <motion.article
+          className="flex flex-col gap-4 py-6 px-8 border border-gray-500 bg-secondary rounded-lg max-w-md w-full"
+          initial={variants.initial}
+          animate={variants.animate}
+          exit={variants.exit}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="flex flex-col items-center gap-4">
+            <h2 className="text-3xl text-blue-500 font-bold">RideNow</h2>
+            <h3 className="text-2xl font-semibold">Registro</h3>
+            <h5 className="text-sm text-gray-500">Crea tu cuenta en RideNow</h5>
+          </div>
+
+          <RegisterForm />
+
+          <button
+            className="text-gray-500 hover:text-white transition"
+            onClick={() => setStep(step - 1)}
+          >
+            Volver
+          </button>
+        </motion.article>
+      </AnimatePresence>
+    </main>
+  );
+}
+
 export default function RegisterPage() {
-  return <div>Register</div>;
+  return (
+    <RegisterProvider>
+      <RegisterPageContent />
+    </RegisterProvider>
+  );
 }
