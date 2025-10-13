04cd0bbd2d7684a04e85cb871f862f643f574c00
OrlandoDev17 <orlandodev1707@gmail.com>
2025-10-13 18:48:46 -0400

    🏠 Actualizar página principal y estilos globales

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,26 @@
-import { Hero } from "@/components/sections/Hero";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+
+import { WelcomeMessage } from "@/components/ui/WelcomeMessage";
+import { Travel } from "@/components/ui/rides/Travel";
+
+export default function RidesPage() {
+  const [selectedButton, setSelectedButton] = useState<string>("request");
+
+  const { user } = useAuth();
+
+  const handleButtonClick = (id: string) => {
+    setSelectedButton(id);
+  };
+
   return (
     <>
-      <Hero />
+      <main className="flex flex-col relative max-w-11/12 mx-auto w-full">
+        <WelcomeMessage />
+        <Travel />
+      </main>
     </>
   );
 }
