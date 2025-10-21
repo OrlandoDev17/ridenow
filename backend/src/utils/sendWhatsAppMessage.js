458625a274158b91348daefd6335b4c992d6745c
OrlandoDev17 <orlandodev1707@gmail.com>
2025-10-21 15:40:39 -0400

    Limpieza: eliminadas claves sensibles del código

--- backend/src/utils/sendWhatsAppMessage.js
+++ backend/src/utils/sendWhatsAppMessage.js
@@ -1,4 +1,6 @@
 const twilio = require("twilio");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
