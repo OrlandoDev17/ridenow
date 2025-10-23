648772c5052467cacf474ecdac9bd2cd6c0c198f
OrlandoDev17 <orlandodev1707@gmail.com>
2025-10-23 17:54:53 -0400

    Hacer la web app de android con capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.ridenow.app",
+  appName: "RideNow",
+  webDir: "out",
+};
+
+export default config;
